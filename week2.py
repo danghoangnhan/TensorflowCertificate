3e4c544af24b8cd4bd50855a54b57c885c1d20ec
Danieldu <daniel@jasslin.com>
2023-03-20 16:24:01 +0800

    complete week1

---
+++ week2.py
@@ -0,0 +1,65 @@
+A = [
+    [1, 2, 3, 4],
+    [-2, 1, -8, 7],
+    [0, 1, 1, 4],
+]
+def displayRow(matrix):
+    for row in matrix:
+        print(row,'\n')
+
+def tranpose(maxtrix):
+    new_result = []
+    for i in range(len(maxtrix[0])):
+        cols = []
+        for j in range(len(maxtrix)):
+            cols.append(maxtrix[j][i])
+        new_result.append(cols)
+    return new_result
+
+def cloneAnTranpose(matrix,value):
+    new_result = []
+    for _ in range(len(matrix[0])):
+        cols = []
+        for _ in range(len(matrix)):
+            cols.append(value)
+        new_result.append(cols)
+    return new_result
+
+def cloneMatrix(matrix,value):
+    new_result = []
+    for _ in range(len(matrix)):
+        cols = []
+        for _ in range(len(matrix[0])):
+            cols.append(value)
+        new_result.append(cols)
+    return new_result
+
+def add2Matrix(A,B):
+    result = cloneMatrix(A,0)
+    for i in range(len(A)):
+        for j in range(len(A[0])):
+            result[i][j] = A[i][j]+B[i][j]
+    return result
+def sub2Matrix(A,B):
+    result = cloneMatrix(A,0)
+    for i in range(len(A)):
+        for j in range(len(A[0])):
+            result[i][j] = A[i][j]-B[i][j]
+    return result
+
+def Mutil2Matrix(A,B):
+    result = cloneMatrix(A,0)
+    for i in range(len(A)):
+        for j in range(len(B[0])):
+                for k in range(len(A[0])):
+                    result[i][j]+=A[i][k]*B[k][j]
+    return result
+
+print("A:",A)
+
+B = tranpose(A)
+print("tranpose of A, B:",B)
+C = add2Matrix(A,A)
+print(" A + A = C:",C)
+D = Mutil2Matrix(A,B)
+print("A * B = D:",D)
